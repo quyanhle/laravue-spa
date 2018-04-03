e71c8893334e774f46d3d8c2660f85d21ada883d
cba_alex <alex@cybridge.jp>
2018-04-03 19:49:06 +0900

    first commit

---
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Auth;
+use Socialite;
+
+/**
+ *  AuthenticationController
+ */
+class AuthenticationController extends Controller
+{
+    public function getSocialRedirect($account)
+    {
+        try {
+            return Socialite::with($account)->redirect();
+
+        } catch (\InvalidArgumentException $e) {
+            return redirect('/login');
+        }
+
+    }
+
+    public function getSocialCallback($account)
+    {
+        $socialUser = Socialite::with($account)->user();
+        $user       = User::where('provider_id', $socialUser->id)->where('provider', $account)->first();
+        if (!$user) {
+            $userData['name']        = $socialUser->getName();
+            $userData['email']       = ($socialUser->getEmail() != '') ? $socialUser->getEmail() : '';
+            $userData['avatar']      = $socialUser->getAvatar();
+            $userData['provider']    = $account;
+            $userData['provider_id'] = $socialUser->getId();
+            $userData['password']    = '';
+
+            $user = User::create($userData);
+        }
+        Auth::login($user);
+        return redirect('/');
+    }
+
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+
+    </head>
+    <body>
+        <a href="/login/facebook">
+            Log In With Facebook
+        </a>
+    </body>
+</html>
