f79b56f809fc62ae8038c770ac09ae2cca0a3411
quyanhle <quyanhle83@gmail.com>
2018-04-08 11:52:56 +0900

    dev Cafe modules

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
+  Route::get('/user', function( Request $request ){
+    return $request->user();
+  });
+  Route::get('/cafes', 'API\CafesController@getCafes');
+  Route::post('/cafes', 'API\CafesController@postNewCafe');
+  Route::get('/cafes/{id}', 'API\CafesController@getCafe');
+});
