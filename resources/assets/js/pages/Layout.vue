31d4fb57be75383370b54bd4b86a839e13bd668b
quyanhle <quyanhle83@gmail.com>
2018-04-21 13:10:41 +0900

    change default lat,long in CafeMap

--- resources/assets/js/pages/Layout.vue
+++ resources/assets/js/pages/Layout.vue
@@ -17,7 +17,7 @@
   	},
   	created() {
   		this.$store.dispatch('loadCafes');
-  		// this.$store.dispatch('loadUser');
+  		this.$store.dispatch('loadUser');
       this.$store.dispatch('loadCurrentLocation');
   	}
 
